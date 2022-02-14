ee7c901bee43e1a205a298523ec840afa0ef0969
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-14 17:50:15 -0500

    added a function to write the file

--- script.js
+++ script.js
@@ -1,2 +1,30 @@
 const inquirer = require('inquirer'); //inquirer package remember to install on command line
 const fs = require('fs'); //Filesystem
+
+const questons = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is the Team Managers name?',
+    },
+    {
+        type: 'input',
+        name: 'ID',
+        message: 'What is the Team Managers ID?',
+    },
+    {
+        type: 'input',
+        name: 'Email',
+        message: 'What is the Team Managers email?',
+    },
+    {
+        type: 'input',
+        name: 'Office Number',
+        message: 'What is the Team Managers Office Number?',
+    }
+]
+
+function writeFile(fileName, answers){ //this takes the answers from the prompt above and plugs them into the README file
+    fs.writeFile(fileName, answers, (err) => 
+        err ? console.log(err) : console.log('Successfully created the Team-Profile') //lets us know the README has been written succesfully 
+        )}
