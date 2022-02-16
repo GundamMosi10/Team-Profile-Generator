ab60ccb560fff92b429bd5b19b8f7c6a6078e3b4
Colin Mosi Cargile <cargile10@gmail.com>
2022-02-15 19:41:33 -0500

    created questions for the team maanger and will follow up with the questions for the prompt menu to see if you want to add another employee. Also deleted the html file I created yesterday

--- script2.js
+++ script2.js
@@ -1,67 +1,109 @@
 const inquirer = require('inquirer'); //inquirer package remember to install on command line
 const fs = require('fs'); //Filesystem
+const manager = require('manager');
+const engineer = require('engineer');
+const intern = require('intern');
+const teamMembers = [];
 
-class employee {
-    constructor(name, id, email){
-        this.name = name;
-        this.id = id; 
-        this.email = email;
-    }
+const promptManager = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?',
+        },
+        {
+            type: 'input',
+            name: 'Id',
+            message: 'Please enter your employee Id',
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: 'Please enter your email address',
+        },
+    ]).then (answers => {
+        const manager = new Manager(answers.name, answers.Id, answers.email, answers.officeNumber)
+        teamMembers.push(manager);
+        promptMenu();
+    })
 }
 
-class manager extends employee{
-    constructor(name, id, email, officeNumber){
-        super(name, id, email);
-        this.officeNumber = officeNumber;
-    }
-    
-}
 
-class engineer extends employee{
-    constructor(name, id, email, github){
-        super(name, id, email);
-        this.github = github; 
-    }
-}
 
-class intern extends employee{
-    constructor(name, id, email, school){
-        super(name, id, email);
-        this.school = school;
-    }
-}
 
-const menuQuestion = [
-    {
-        type: 'list',
-        name: 'employeeType',
-        choices: ['Manager', 'Engineer', 'Intern', 'None'],
-        message: 'What kind of employee is this?',
-    }
-]
 
-const employeeQuestions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'What is this employees name?',
-    },
-    {
-        type: 'input',
-        name: 'id',
-        message: 'What is your id number?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is your email address?',
-    }
-]
 
-const engineerQuestions = [
-    {
-        type: 'input',
-        name: 'githubName',
-        message: 'What is your github user name?',
-    }
-]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const menuQuestion = [
+//     {
+//         type: 'list',
+//         name: 'employeeType',
+//         choices: ['Manager', 'Engineer', 'Intern', 'None'],
+//         message: 'What kind of employee is this?',
+//     }
+// ]
+
+// const employeeQuestions = [
+//     {
+//         type: 'input',
+//         name: 'name',
+//         message: 'What is this employees name?',
+//     },
+//     {
+//         type: 'input',
+//         name: 'id',
+//         message: 'What is your id number?',
+//     },
+//     {
+//         type: 'input',
+//         name: 'email',
+//         message: 'What is your email address?',
+//     }
+// ]
+
+// const engineerQuestions = [
+//     {
+//         type: 'input',
+//         name: 'githubName',
+//         message: 'What is your github user name?',
+//     }
+// ]
+
+// const interQuestions = [
+//     {
+//         type: 'input',
+//         name: 'school', 
+//         message: 'What is the school that you go to?',
+//     }
+// ]
